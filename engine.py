13385ae197ecc268115365d92806501a26fbde51
ixneisdgnsymv <140933841+ixneisdgnsymv@users.noreply.github.com>
2024-08-22 19:33:54 +0800

    Update engine.py

--- engine.py
+++ engine.py
@@ -1,23 +1,64 @@
 import tkinter as tk
+import time
 from draw_3d import *
-cam_x = 0
-cam_y = 0
-cam_z = 0
-rot_x = 0
-rot_y = 0
-def on_key_press():
-    if event.keysym == 'w':
-        cam_z += 5.0
-    elif event.keysym == 's':
-        cam_z -= 5.0
-    elif event.keysym == 'a':
-        cam_x -= 5.0
-    elif event.keysym == 'd':
-        cam_x += 5.0
-    elif event.keysym == 'Shift_L' or event.keysym == 'Shift_R':
-        cam_y -= 5.0
-    elif event.keysym == 'Space':
-        cam_y += 5.0
+class Game():
+    def __init__(self, root):
+        self.root = root
+        self.root.title('Minecraft Python Editon')
+        self.root.geometry('800x600')
+        self.root.bind('<Key>', self.update)
+        self.canvas = tk.Canvas(self.root, width=800, height=600)
+
+        self.is_running = False
+        self.seed = 0
+        self.FPS = 0
+        
+        self.start_time = 0
+        self.end_time = 0
+        
+        self.player_x = 0
+        self.player_y = 0
+        self.player_z = -100
+        self.player_x_dir = 0
+        self.player_y_dir = 0
+
+        self.walk_speed = 5.0
+        self.swim_speed = 3.0
+    def update(self， event):
+        if event.keysym == 'w':
+            self.player_z += 50.0
+        elif event.keysym == 's':
+            self.player_z -= 50.0
+        elif event.keysym == 'a':
+            self.player_x -= 50.0
+        elif event.keysym == 'd':
+            self.player_x += 50.0
+        elif event.keysym == 'Shift_L' or event.keysym == 'Shift_R':
+            self.player_y -= 50.0
+        elif event.keysym == 'Space':
+            self.player_y += 50.0
+        
+        self.draw_square(0, 0, 0, 50, (self.player_x, self.player_y, self.player_z), (self.player_x_dir, self.player_y_dir))
+    def draw_square(self, x, y, z, s, cam_pos, rot, d=300):
+        draw_3d.draw_3d_line(self.canvas, (x-s, y+s, z-s), (x+s, y+s, z-s), cam_pos, d, rot)
+        draw_3d.draw_3d_line(self.canvas, (x+s, y-s, z-s), (x+s, y+s, z-s), cam_pos, d, rot)
+        draw_3d.draw_3d_line(self.canvas, (x+s, y-s, z-s), (x-s, y-s, z-s), cam_pos, d, rot)
+        draw_3d.draw_3d_line(self.canvas, (x-s, y+s, z-s), (x-s, y-s, z-s), cam_pos, d, rot)
+
+        draw_3d.draw_3d_line(self.canvas, (x-s, y+s, z+s), (x+s, y+s, z+s), cam_pos, d, rot)
+        draw_3d.draw_3d_line(self.canvas, (x+s, y-s, z+s), (x+s, y+s, z+s), cam_pos, d, rot)
+        draw_3d.draw_3d_line(self.canvas, (x+s, y-s, z+s), (x-s, y-s, z+s), cam_pos, d, rot)
+        draw_3d.draw_3d_line(self.canvas, (x-s, y+s, z+s), (x-s, y-s, z+s), cam_pos, d, rot)
+
+        draw_3d.draw_3d_line(self.canvas, (x+s, y+s, z-s), (x+s, y+s, z+s), cam_pos, d, rot)
+        draw_3d.draw_3d_line(self.canvas, (x+s, y-s, z-s), (x+s, y-s, z+s), cam_pos, d, rot)
+        draw_3d.draw_3d_line(self.canvas, (x-s, y+s, z-s), (x-s, y+s, z+s), cam_pos, d, rot)
+        draw_3d.draw_3d_line(self.canvas, (x-s, y-s, z-s), (x-s, y-s, z+s), cam_pos, d, rot)
+
+
 def main():
     root = tk.Tk()
+    game = Game(root)
     root.mainloop()
+if '__name__' == '__main__':
+    main()
